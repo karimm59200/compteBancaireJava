24628fc3a0a8815f922b93acb8bc3eb9ca6fe1a6
karimm59200 <karimm991@gmail.com>
2023-04-09 12:54:40 +0200

    commit exercice compte bancaire

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,19 @@
-package org.example;public class Main {
+package org.example;
+
+import org.example.compte.BankAccount;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
+
+        BankAccount compte1 = new BankAccount(1_123_123_123,  "karim megri");
+
+        compte1.deposit(150);
+
+        compte1.withdrawMoney(50);
+
+       compte1.DisplayAccount();
+
+        System.out.println(compte1);
     }
 }
--- src/main/java/org/example/compte/BankAccount.java
+++ src/main/java/org/example/compte/BankAccount.java
@@ -1,2 +1,39 @@
-package org.example.compte;public class BankAccount {
+package org.example.compte;
+
+public class BankAccount {
+
+     private int accountNum;
+     private double sold;
+
+     private String owner;
+
+
+
+     public BankAccount(int accountNum,  String owner){
+          this.accountNum = accountNum;
+          this.sold = 0;
+          this.owner = owner;
+     }
+
+
+     public void deposit( int amount){
+           sold = sold +  amount;
+     }
+
+     public void withdrawMoney(int amount){
+          sold = sold - amount;
+     }
+
+     public void DisplayAccount(){
+          System.out.println( " le compte : " + accountNum + " de monsieur "+ owner + " a un solde de : " + sold + " euros");
+     }
+
+     @Override
+     public String toString() {
+          return "BankAccount{" +
+                  "accountNum=" + accountNum +
+                  ", sold=" + sold +
+                  ", owner='" + owner + '\'' +
+                  '}';
+     }
 }
